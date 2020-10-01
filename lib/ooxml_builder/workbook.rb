a23e24c6d6e5e47d4013af2c597fa0f9c5b2049d
mollerhoj <mollerhoj3@gmail.com>
2020-10-01 17:27:08 +0200

    update desired_outcome

---
+++ lib/ooxml_builder/workbook.rb
@@ -0,0 +1,19 @@
+require 'zip/filesystem'
+require 'fileutils'
+require 'tmpdir'
+
+module OoxmlBuilder
+  class Workbook
+    include OoxmlBuilder::Util
+
+    # TODO Should be able to generate Excel. Copy code from presentation.rb
+
+
+
+
+    # Same as save from presenation.rb
+    def save
+    end
+
+  end
+end
