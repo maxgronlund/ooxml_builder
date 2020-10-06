8fee8bc40cf52fbee5ba4806e4cf5c92039cbc5e
Max Grønlund <maxgronlund@bigmax.local>
2020-10-06 09:46:40 +0200

    updating the test.sh

--- spec/test.sh
+++ spec/test.sh
@@ -1,8 +1,20 @@
 !#/bin/bash
 
+
 rm -rf ../samples/pptx/*
+
 cd ../
 rspec spec
 cd samples/pptx
 cp  sample.pptx sample.zip
 unzip sample.zip
+cd ppt/embeddings
+
+
+zip -r Microsoft_Excel_Worksheet.zip Microsoft_Excel_Worksheet
+cp Microsoft_Excel_Worksheet.zip Microsoft_Excel_Worksheet-test.xlsx
+
+# open 'Microsoft_Excel_Worksheet1.xml'
+
+ls -al
+# zip -r Microsoft_Excel_Worksheet.zip Microsoft_Excel_Worksheet
