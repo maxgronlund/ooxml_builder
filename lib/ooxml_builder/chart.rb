c2c61bad4d9463406ef6684501c90f60bd737e9e
Max Grønlund <maxgronlund@bigmax.local>
2020-10-09 09:08:21 +0200

    moved generation of sheets into new class

---
+++ lib/ooxml_builder/chart.rb
@@ -0,0 +1,37 @@
+require 'zip/filesystem'
+require 'fileutils'
+require 'tmpdir'
+require 'awesome_print'
+
+module OoxmlBuilder
+  class Chart
+    include OoxmlBuilder::Util
+
+    def initialize(options={})
+      require_arguments [:content], options
+      options.each { |k, v| instance_variable_set("@#{k}", v) }
+    end
+
+    def save(extract_path, index)
+      save_rel_xml(extract_path, index)
+      save_chart_xml(extract_path, index)
+    end
+
+    def save_rel_xml(extract_path, index)
+      render_view('chart_rel.xml.erb', "#{extract_path}/ppt/charts/_rels/chart#{index}.xml.rels", index: index)
+    end
+    private :save_rel_xml
+
+    def save_chart_xml(extract_path, index)
+      # remove first row
+      @content[:data].delete(:Column1)
+      params = { rows: @content[:data], suffix: @content[:suffix] }
+
+      render_view('chart.xml.erb', "#{extract_path}/ppt/charts/chart#{index}.xml", params)
+
+
+      # render_view('chart.xml.erb', "#{extract_path}/ppt/embeddings/Microsoft_Excel_Worksheet#{index}.xml")
+    end
+      private :save_chart_xml
+  end
+end
