e9fa6b3c6bdeb628a60727a9a11c0c53b5f731b5
Max Grønlund <maxgronlund@bigmax.local>
2020-10-09 09:02:43 +0200

    removed unused files

---
+++ spec/unpack_output_spec.rb
@@ -0,0 +1,13 @@
+require 'ooxml_builder'
+require_relative 'test_fixture.rb'
+require 'awesome_print'
+
+describe 'OoxmlBuilder.compression' do
+
+
+  it 'Create a PPTX file from a folder.' do
+
+  end
+
+
+end
