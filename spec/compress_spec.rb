22805a78420487d1f4903230d8b751e6831f9c7a
Max Grønlund <maxgronlund@bigmax.local>
2020-10-12 09:56:02 +0200

    added a bar_slide_test

--- spec/compress_spec.rb
+++ spec/compress_spec.rb
@@ -1,6 +1,5 @@
 require 'ooxml_builder'
 require_relative 'test_fixture.rb'
-require 'awesome_print'
 
 describe 'OoxmlBuilder.compression create a xlsx file' do
   it 'Create a XLSX file from a folder.' do
